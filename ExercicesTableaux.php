d7f36b51050ef5a598b924741170c0d07f5ca086
Justine Serra <bridel44@hotmail.fr>
2026-06-09 10:24:34 +0200

    en cours

---
+++ ExercicesTableaux.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercices PHP - Tableaux (indexés et associatifs) </title>
+</head>
+<body>
+<?php 
+// Partie 1 : Tableaux indexés (ordonnés)
+
+//Exercice 1.1 
+// $fruits = ["pomme", "banane", "orange"];
+// foreach($fruits as $fruit) { //passe en revue le tableau $fruits, fruit en cours stocké dans la variable $fruit
+//     echo $fruit ."<br>";
+// }
+
+//Exercice 1.2
+// $fruits = ["pomme", "banane", "orange"];
+// echo "Premier fruit : " . $fruits[0] . "<br>"; //l'index commence toujours par 0
+// echo "Dernier fruit : " . $fruits[2] . "<br>";
+
+//Exercice 1.3
+// $fruits = ["pomme", "banane", "orange"];
+// $fruits[] = "kiwi"; // [] = permet d'ajouter des nouveaux éléments à la fin du tableau
+// $fruits[] = "ananas"; 
+// foreach ($fruits as $fruit) {
+//     echo $fruit . "<br>";
+// }
+
+//Exercice 1.4
+
+
+?>  
+    
+</body>
+</html>
