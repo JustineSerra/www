08aec62546bceb89ee2c421bc75c9900e2de5744
Justine Serra <bridel44@hotmail.fr>
2026-06-10 11:21:40 +0200

    en cours

--- ExercicesCourantes.php
+++ ExercicesCourantes.php
@@ -31,6 +31,43 @@
 // }
 
 //Exercice 1.4
+// $phrase = "Se lever le matin c'est difficile";
+// $nouvellePhrase = str_replace("difficile", "facile", $phrase);
+
+// echo $nouvellePhrase;
+
+// 2. Fonctions sur les nombres
+
+//Exercice 2.1
+// $nombre = 4.56;
+// echo round($nombre);
+// echo "<br>";
+// echo floor($nombre);
+// echo "<br>";
+// echo ceil($nombre);
+
+//Exercice 2.2
+// $number=rand(1,100);
+// echo $number;
+
+//Exercice 2.3
+// $prix = 1234.5635;
+// $prixDecimale = number_format($prix, 2, ",", "");
+// echo "le prix en decimale est $prixDecimale €";
+
+//3. Fonctions sur les booléens 
+
+//Exercice 3.1
+// $valeur = 0;
+// if (empty($valeur)) {
+//     echo "La variable est vide";
+// } else {
+//     echo "La variable contient une valeur.";
+// }
+
+//Exercice 3.2
+
+
 
 
 
