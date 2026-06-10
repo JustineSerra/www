f972bc6ea03422fe9bd5f46b6e327872737f37a5
Justine Serra <bridel44@hotmail.fr>
2026-06-10 10:51:26 +0200

    en cours

--- ExercicesCourantes.php
+++ ExercicesCourantes.php
@@ -11,6 +11,26 @@
 //1.Fonctions sur les chaînes de caractères
 
 //Exercice 1.1
+// $texte = "Salut à tous c'est Squeezie";
+// echo strLen($texte);
+
+//Exercice 1.2
+// $texte = "test d'une phrase";
+// echo strtoupper($texte);
+// echo "<br>";
+// echo strtolower($texte);
+
+//Exercice 1.3
+// $phrase = "Je fais actuellement du PHP";
+// $motRecherche="PHP";
+// $position = strpos($phrase, $motRecherche);
+// if ($position !== false){
+//     echo "Le mot $motRecherche a été trouvé. Il commence à la position : $position.";
+// } else {
+//     echo "Le mot $motRecherche n'a pas été trouvé dans la phrase.";
+// }
+
+//Exercice 1.4
 
 
 
