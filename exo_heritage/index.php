0ebea02b6c0b9937bbf07aaa1b42cda5fdcc6986
Justine Serra <bridel44@hotmail.fr>
2026-06-25 09:53:23 +0200

    en cours

--- exo_heritage/index.php
+++ exo_heritage/index.php
@@ -19,15 +19,35 @@ $monAnimal->decrire();
 
 //Exo 2
 class Chien extends Animal {
+
     public function aboyer() {
         echo "Wouf! <br>";
+        //exo 3
     }
+
+        public function decrire() {
+        echo "Je suis un chien et je m'appelle " . $this->nom . "<br>";
+    } //observation : PHP ignore la méthode parent et execute $monChien->decrire de la classe Chien
 
 }
 
 $monChien = new Chien("Loulou", "Yorkshire");
-$monChien->aboyer();
+$monChien->aboyer(); //exo 3
 $monChien->decrire();
 
+//exo 5
+class Chat extends Animal {
+    public function miauler() {
+        echo "Miaou <br>";
+    }
+
+    public function decrire() {
+        echo "Je suis un chat et je m'appelle " . $this->nom . "<br>";
+    }
+}
+
+$monChat=new Chat("Simba", "Siamois");
+$monChat->miauler();
+$monChat->decrire();
 
 ?>
